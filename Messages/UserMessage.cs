a2337a88d0ba969603d2e610052da16835dcfe5b
Scott Thomas <kidfearless1@gmail.com>
2025-04-28 17:28:08 -0600

    add helper method

--- Messages/UserMessage.cs
+++ Messages/UserMessage.cs
@@ -6,4 +6,14 @@ public record UserMessage(string Content) : Message(Content)
 {
     public override string Role { get; set; } = "user";
     public List<AIContent> Files { get; set; } = [];
+
+    public UserMessage WithFiles(params DataContent[] content)
+    {
+        return this with { Files = [.. this.Files, .. content] };
+    }
+
+    public UserMessage WithFile(DataContent content)
+    {
+        return this with { Files = [content] };
+    }
 }
